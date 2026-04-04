4c95060bb49beb5a39d4846ebc3c0e8ecdc364cd
XVYMMC <tobal.morales.m@gmail.com>
2026-04-04 18:36:50 -0300

    modificado server.py

--- app/backend/src/server.py
+++ app/backend/src/server.py
@@ -9,5 +9,5 @@ app = Flask(__name__)
 
 app.register_blueprint(search_bp, url_prefix='/api/search')
 
-if __name__ == '__main__':
-    app.run(host='localhost', port=3000)
+if __name__ == "__main__":
+    app.run(host="localhost", port=3000)
