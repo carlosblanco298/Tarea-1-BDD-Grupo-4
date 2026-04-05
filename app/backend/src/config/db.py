54a6803b76390a979fcca32d25d26dff52f79fac
Matthew Babbitt <matthewbabbitt05@gmail.com>
2026-04-05 18:33:35 -0400

    rutas, controller, server, config

--- app/backend/src/config/db.py
+++ app/backend/src/config/db.py
@@ -1,13 +1,20 @@
-
+import os
 from psycopg2 import pool
 from dotenv import load_dotenv
 
-try:
-conn = psycopg2.connect(
-database=“database”,
-user=“user”,
-host=“localhost”,
-port=5432,
-password=“pass”)
-except:
-print(“No me pude conectar”)
+load_dotenv()
+
+connection_pool = pool.ThreadedConnectionPool(
+    1, 20,
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
+
+def get_db():
+    return connection_pool.getconn()
+
+def release_db(conn):
+    connection_pool.putconn(conn)
--- app/backend/src/server.py
+++ app/backend/src/server.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from dotenv import load_dotenv
-
-from routes.searchRoute import search_bp
+from routes.sponsorsRoute import sponsor_bp
 
 load_dotenv()
 
 app = Flask(__name__)
-
-app.register_blueprint(search_bp, url_prefix='/api/search')
+app.register_blueprint(sponsor_bp, url_prefix='/api/sponsors')
 
 if __name__ == "__main__":
     app.run(host="localhost", port=3000)
