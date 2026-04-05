10f9a7ca4987b6c27aad07daa0f980d8c72cb62d
Matthew Babbitt <matthewbabbitt05@gmail.com>
2026-04-04 23:30:17 -0400

    alo

--- app/backend/src/config/db.py
+++ app/backend/src/config/db.py
@@ -1,23 +1,13 @@
-import os
+
 from psycopg2 import pool
 from dotenv import load_dotenv
 
-load_dotenv()
-
-# Inicializamos el pool una vez que la app inicia
-connection_pool = pool.ThreadedConnectionPool(
-    1, 20,
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"),
-    database=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD")
-)
-
-def get_db():
-    # Obtener una conexión al pool
-    return connection_pool.getconn()
-
-def release_db(conn):
-    # Devolver la conexión al pool en lugar de cerrarla
-    connection_pool.putconn(conn)
+try:
+conn = psycopg2.connect(
+database=“database”,
+user=“user”,
+host=“localhost”,
+port=5432,
+password=“pass”)
+except:
+print(“No me pude conectar”)
