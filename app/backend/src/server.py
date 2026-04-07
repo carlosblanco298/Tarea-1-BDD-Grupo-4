8f40bd67e6b7c082fc0921e681d1d65bf89e2b1c
Matthew Babbitt <matthewbabbitt05@gmail.com>
2026-04-07 19:02:15 -0400

    veamos

--- app/backend/src/server.py
+++ app/backend/src/server.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 from routes.sponsorsRoute import sponsor_bp
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, 
+    template_folder='../../frontend/src/templates',
+    static_folder='../../frontend/src/static'
+)
 app.register_blueprint(sponsor_bp, url_prefix='/api/sponsors')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sponsors')
+def sponsors():
+    return render_template('sponsors.html')
 
 if __name__ == "__main__":
     app.run(host="localhost", port=3000)
