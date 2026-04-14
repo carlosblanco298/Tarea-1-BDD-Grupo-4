3bb0dcd1041930372dfa2d372c76a5619ef5f526
carlosblanco298 <blanco.s.carlos06@gmail.com>
2026-04-13 23:47:33 -0400

    Add abs path

--- app/backend/src/server.py
+++ app/backend/src/server.py
@@ -1,12 +1,15 @@
+import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
 from routes.sponsorsRoute import sponsor_bp
 
-load_dotenv()
+base_dir = os.path.dirname(os.path.abspath(__file__)) # This is /backend/src
+template_dir = os.path.join(base_dir, '..', '..', 'frontend', 'src', 'templates')
+static_dir = os.path.join(base_dir, '..', '..', 'frontend', 'src', 'static')
 
 app = Flask(__name__, 
-    template_folder='../../frontend/src/templates',
-    static_folder='../../frontend/src/static'
+    template_folder=template_dir,
+    static_folder=static_dir
 )
 app.register_blueprint(sponsor_bp, url_prefix='/api/sponsors')
 
